707bc4271b747476a7dfd832c15a3a8d1556d3a7
mayurilamkane <mayurilamakne8@gmail.com>
2020-01-07 21:57:32 +0530

    --commit -CM_ProjectList

---
+++ app/src/main/java/be/project/sitereck/Construction_Manager/ProjectDataClass.java
@@ -0,0 +1,70 @@
+package be.project.sitereck.Construction_Manager;
+
+class ProjectDataClass {
+    private String Title ,StartDate,EndDate,Id;
+    private String lat,lon;
+    private int image;
+
+    public ProjectDataClass(String title, String startDate, String endDate, String id) {
+        Title = title;
+        StartDate = startDate;
+        EndDate = endDate;
+        Id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String startDate) {
+        StartDate = startDate;
+    }
+
+    public String getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(String endDate) {
+        EndDate = endDate;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/be/project/sitereck/Construction_Manager/ProjectList_CM.java
@@ -0,0 +1,32 @@
+package be.project.sitereck.Construction_Manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.List;
+
+import be.project.sitereck.R;
+
+public class ProjectList_CM extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<ProjectDataClass> list; // Create list for data class
+    String title,startdate[],endDate[];
+    LinearLayout ongoing_lay,all_lay,profile_lay;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_list__cm);
+
+        initElements();
+    }
+
+    private void initElements() {
+        //use this method to initialize every view element
+    }
+}
