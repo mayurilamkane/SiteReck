3eff1aaebab4b92395f973642dfe1d7ae61a14e5
mayurilamkane <mayurilamakne8@gmail.com>
2020-02-06 13:10:27 +0530

    CM UI initial

--- app/src/main/java/be/project/sitereck/Construction_Manager/DataClass/ProjectDataClass.java
+++ app/src/main/java/be/project/sitereck/Construction_Manager/DataClass/ProjectDataClass.java
@@ -1,17 +1,60 @@
 package be.project.sitereck.Construction_Manager.DataClass;
 
 public class ProjectDataClass {
-    public ProjectDataClass(String head, String desc) {
-        this.head = head;
-        this.desc = desc;
-    }
-    private String head,desc;
 
-    public String getHead() {
-        return head;
+    String Title ,StartDate,EndDate,Id;
+    String lat,lon;
+    int image;
+
+    public ProjectDataClass(){
+        Title = Title;
+        StartDate = StartDate;
+        EndDate = EndDate;
+        //this.Id = Id;
     }
 
-    public String getDesc() {
-        return desc;
+    public ProjectDataClass(String proj_name, String proj_start_date, String proj_end_date, String proj_id) {
+        Title = proj_name;
+        StartDate = proj_start_date;
+        EndDate = proj_end_date;
+        this.Id = proj_id;
     }
+    public ProjectDataClass(String proj_name, String proj_start_date, String proj_end_date, String proj_id,String lat ,String lon) {
+        Title = proj_name;
+        StartDate = proj_start_date;
+        EndDate = proj_end_date;
+        this.Id = proj_id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public String getEndDate() {
+        return EndDate;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
 }
