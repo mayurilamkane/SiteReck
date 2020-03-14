c531024c6a28c938af6baf4cec935fd0eca87c76
yuvrajvansure <yuvrajvansure@gmail.com>
2020-03-14 15:43:34 +0530

    cm list for pm

---
+++ app/src/main/java/be/project/sitereck/ProjectManager/Activities/AllCmList_PM.java
@@ -0,0 +1,16 @@
+package be.project.sitereck.ProjectManager.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import be.project.sitereck.R;
+
+public class AllCmList_PM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_cm_list_pm);
+    }
+}
