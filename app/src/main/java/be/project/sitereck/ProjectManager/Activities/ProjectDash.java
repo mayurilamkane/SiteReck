97bdd774832283cd2293e1f6f79875213944fea4
YuvrajVansure <yuvrajvansure@gmail.com>
2020-03-22 22:38:25 +0530

    ProjectDash initialized

---
+++ app/src/main/java/be/project/sitereck/ProjectManager/Activities/ProjectDash.java
@@ -0,0 +1,40 @@
+package be.project.sitereck.ProjectManager.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import be.project.sitereck.R;
+
+public class ProjectDash extends AppCompatActivity implements View.OnClickListener{
+    Button btnActivity,btnCM,btnReq;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_dash);
+
+        btnActivity = findViewById(R.id.btAct);
+        btnCM = findViewById(R.id.btCm);
+        btnReq = findViewById(R.id.btReq);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btAct:
+                Intent intent = new Intent(this,ActivityManager_PM.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btCm:
+                break;
+
+            case R.id.btReq:
+                break;
+
+        }
+
+    }
+}
